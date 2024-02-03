087df58fb7bb50ee4e88bd84897c2db7eca375d2
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-02-03 13:16:30 -0500

    added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import LandingPage from "./components/landingPage";
 import Header from "./components/Header";
 import { useState } from "react";
 import HomePage from "./components/homePage";
+import SpacePage from "./components/spacePage";
+import ExplorePage from "./components/explorePage";
+import NearPage from "./components/nearPage";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -14,6 +17,9 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<HomePage />} />
+          <Route path="/space" element={<SpacePage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/near" element={<NearPage />} />
         </Routes>
       </div>
     </>
