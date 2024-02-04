c66776d277d053f4e43cc92be27896a9c83e17a2
amritav <amritavisalam@gmail.com>
2024-02-03 21:35:15 -0500

    create collection modal

--- frontend/src/components/modals/CreateCollectionModal.tsx
+++ frontend/src/components/modals/CreateCollectionModal.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateCollectionModal = () => {
   const [showModal, setShowModal] = React.useState(false);
+  const [collectionName, setCollectionName] = useState("");
+  const [collectionDescription, setCollectionDescription] = useState("");
+
   return (
     <>
       <button
@@ -13,7 +16,7 @@ const CreateCollectionModal = () => {
         }}
         onClick={() => setShowModal(true)}
       >
-        Create Collection
+        Create a Collection
       </button>
 
       {showModal ? (
@@ -22,38 +25,67 @@ const CreateCollectionModal = () => {
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             onClick={() => setShowModal(false)}
           >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
+            <div
+              className="relative my-6 mx-auto w-2/6"
+              onClick={(e) => e.stopPropagation()} 
+            >
+              {/* content */}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between px-6 py-4  rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                {/* header */}
+                <div className="flex items-center justify-center px-6 py-4 rounded-t">
+                  <h3 className="text-2xl font-bold">Create a Collection</h3>
                 </div>
-                {/*body*/}
+                <hr className="mb-4" />
+                {/* body */}
                 <div className="relative px-6 flex-auto">
-                  <p className="text-lg leading-relaxed">
-                    I always felt like I could do anything. That's the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can't do anything, you
-                    won't do anything. I was taught I could do everything.
-                  </p>
+                  <div className="mb-4">
+                    <label htmlFor="collectionName" className="block text-gray-700 text-sm font-medium mb-2">
+                      Give your collection a name
+                    </label>
+                    <input
+                      id="collectionName"
+                      type="text"
+                      value={collectionName}
+                      onChange={(e) => setCollectionName(e.target.value)}
+                      className=" appearance-none py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      placeholder="untitled list"
+                      style={{
+                        border: "none",
+                        borderBottom: "2px solid gray", 
+                        borderRadius: "0",
+                        width: "100%",
+                      }}
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <label htmlFor="collectionDescription" className="block text-gray-700 text-sm font-medium mb-2">
+                      Describe your collection
+                    </label>
+                    <textarea
+                      id="collectionDescription"
+                      value={collectionDescription}
+                      onChange={(e) => setCollectionDescription(e.target.value)}
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      placeholder="collection description"
+                    ></textarea>
+                  </div>
                 </div>
-                {/*footer*/}
+                {/* footer */}
                 <div className="flex items-center justify-end p-4 ">
                   <button
-                    className="bg-white text-black border-2 border-black rounded-md  px-4 py-1 mx-1"
+                    className="bg-white text-black border-2 border-black rounded-md px-4 py-1 mx-1"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Cancel
                   </button>
                   <button
-                    className="bg-black text-white border-2 border-black rounded-md  px-4 py-1 mx-1"
+                    className="bg-black text-white border-2 border-black rounded-md px-4 py-1 mx-1"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    Save Changes
+                    Create
                   </button>
                 </div>
               </div>
