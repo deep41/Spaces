d5b8e9e2308a47f64d393f4e15b053ca8eb5a8df
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-02-04 05:42:09 -0500

    hid header properly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/landingPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import HomePage from "./components/homePage";
 import ExplorePage from "./components/explorePage";
 import NearPage from "./components/nearPage";
@@ -11,12 +11,16 @@ import CommunitySearchPage from "./components/CommunitySearchPage";
 
 function App() {
   // Check if the current route is the landing page
-  const isLandingPage = window.location.pathname === "/";
+  const loc = useLocation();
+  const [isLanding, setLanding] = useState(window.location.pathname === "/");
+  useEffect(() => {
+    setLanding(window.location.pathname === "/");
+  }, [loc]);
 
   return (
     <>
       <div>
-        {!isLandingPage && <Header />}
+        {!isLanding && <Header />}
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<HomePage />} />
